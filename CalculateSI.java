affded2b6c82f76d7ee42ba6fbb64a65d2134647
CodingJava37 <139805366+CodingJava37@users.noreply.github.com>
2023-07-17 23:48:27 +0530

    Create CalculateSI.java

---
+++ CalculateSI.java
@@ -0,0 +1,9 @@
+public class CalculateSI {
+    public static void main(String[] args) {
+        int p = 100;
+        int r = 7;
+        int t = 2;
+        double si = (p*r*t)/100;
+        System.out.println(si);
+    }
+}
