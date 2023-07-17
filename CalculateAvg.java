e9742f7eb7455b51b6162ce25d86b44a65a4f903
CodingJava37 <139805366+CodingJava37@users.noreply.github.com>
2023-07-17 23:47:57 +0530

    Create CalculateAvg.java

---
+++ CalculateAvg.java
@@ -0,0 +1,9 @@
+public class CalculateAvg {
+    public static void main(String[] args) {
+        int x =10;
+        int y =12;
+        int z = 15;
+        double avg = (x+y+z)/3;
+        System.out.println("This is avg of given numbers: " + avg );
+    }
+}
